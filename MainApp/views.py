af9587899c7b11d62eb186f85241854cdac6ee19
Anton Chunin <antonc89@gmail.com>
2025-04-11 01:42:01 +0300

    Done. M1:task4-6

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
+from django.http import HttpResponse, HttpResponseNotFound
 
 def home(request):
     text = """
@@ -36,23 +34,20 @@ items = [
    {"id": 8, "name": "Кепка", "quantity": 124},
 ]
 
-# def item1(request, item):
-#     count_1 = 0
-#     for product in items:
-#         count_1 += 1
-#         if product.get("id") == item:
-#             return HttpResponse(f'{product.get("name"):} {product.get("quantity")} штук <br><a href="http://127.0.0.1:8000/items"> Назад к списку товаров</a>')
-#         elif count_1 > len(items):
-#             break
-    # if item not in items:
-    #     return HttpResponse(f'<h1> Товар с {product.get("id")} не найден <a href="http://127.0.0.1:8000/items"> Назад к списку товаров</a></h1>')\
-
 def get_item(request, item_id):
     for item in items:
         if item['id'] == item_id:
             result = f"""
             <h2> Название: {item["name"]} </h2>
             <p> Количество: {item["quantity"]} </p>
+            <p> <a href="/items"> Назад к списку товаров </a></p>
             """
             return HttpResponse(result)
     return HttpResponseNotFound(f"Товар с id={item_id} не найден")
+
+def items_list(request):
+    result = "<h1> Список товаров: </h1><ol>"
+    for item in items:
+        result += f"""<li><a href="/item/{item['id']}"> {item['name']} </li>"""
+    result += "</ol>"
+    return HttpResponse(result)
