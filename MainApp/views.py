58ce2c2267451c8ba40bfcad0742a1e35de3bea8
Anton Chunin <antonc89@gmail.com>
2025-04-10 23:59:09 +0300

    Done. M1:task2

--- MainApp/views.py
+++ MainApp/views.py
@@ -3,9 +3,27 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+author = {
+    "Имя": "Антон",
+    "Отчество": "Андреевич",
+    "Фамилия": "Чунин",
+    "телефон": "8-904-008-57-08",
+    "email": "antonc89@gmail.com"
+}
+
 def home(request):
     text = """
     <h1>"Изучаем django"</h1>
     <strong>Автор</strong>: <i>Чунин А.А.</i>
+    """
+    return HttpResponse(text)
+
+def about(request):
+    text = f"""
+    Имя: <b>{author["Имя"]}</b><br>
+    Отчество: <b>{author["Отчество"]}</b><br>
+    Фамилия: <b>{author['Фамилия']}</b><br>
+    телефон: <b>{author['телефон']}</b><br>
+    email: <b>{author['email']}</b><br>
     """
     return HttpResponse(text)
