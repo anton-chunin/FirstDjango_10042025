63875b5a4d9a3a986cef7818bfc35aa9a6e635d7
Anton Chunin <antonc89@gmail.com>
2025-04-15 20:28:12 +0300

    Done.M3 part2:task1-3

--- MainApp/models.py
+++ MainApp/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 
-
 class Item(models.Model):
     name  = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
-    count = models.PositiveIntegerField() 
+    count = models.PositiveIntegerField()
+    description = models.TextField(max_length=200, default='Описание элемента')
 
