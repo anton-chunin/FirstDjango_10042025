41d3f43533301f8b9fd66d28be28dc50586dd75a
Anton Chunin <antonc89@gmail.com>
2025-04-15 19:47:31 +0300

    Done.M3:task3-4

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,18 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 from MainApp.models import Item
-
-# Create your views here
-
-
-# items = [
-#    {"id": 1, "name": "Кроссовки abibas", "quantity": 5},
-#    {"id": 2, "name": "Куртка кожаная", "quantity": 2},
-#    {"id": 5, "name": "Coca-cola 1 литр", "quantity": 12},
-#    {"id": 7, "name": "Картофель фри", "quantity": 0},
-#    {"id": 8, "name": "Кепка", "quantity": 124},
-# ]
-
+from django.core.exceptions import ObjectDoesNotExist
 
 def home(request):
     context = { 
@@ -34,14 +23,14 @@ def about(request):
 
 
 def get_item(request, item_id: int):
-    item = next((item for item in items if item['id'] == item_id), None)
-
-    if item is not None:
-        context = {
-            "item": item
-        }
+    try:
+        item = Item.objects.get(id=item_id)
+    except ObjectDoesNotExist:
+        return HttpResponseNotFound(f"Item with id={item_id} not found")
+    else:
+        context = {"item": item}
         return render(request, "item_page.html", context)
-    return HttpResponseNotFound(f"Item with id={item_id} not found") 
+     
                                 
  
 def get_items(request):
